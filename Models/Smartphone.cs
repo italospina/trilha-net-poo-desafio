047e4932b014084308c0006f7644fd4f5c912e00
Italo Spina <italo.spina@hotmail.com>
2024-10-04 00:00:57 -0300

    Atualização Outubro

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,14 +3,28 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+
+        private string Modelo { get; set; }
+
+        private string IMEI { get; set; }
+
+        private int Memoria { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
-        }
+            Console.WriteLine($"Modelo: {modelo} \n" +
+                $"Numero: {numero} \n" +
+                $"IMEI: {imei} \n" +
+                $"Memoria: {memoria}");
 
+        }
+        
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "11994455661", modelo: "nokia", imei: "8888888888", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+
+Smartphone iphone = new Iphone(numero: "159955778811", modelo: "Iphone", imei: "31313131313", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
